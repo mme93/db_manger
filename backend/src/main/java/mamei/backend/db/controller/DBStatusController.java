4e653e7d51e35e6c4310f7933f8c747181152df7
magantus@googlemail.com <Extrem93!>
2023-09-14 23:17:14 +0200

    update

--- backend/src/main/java/mamei/backend/db/controller/DBStatusController.java
+++ backend/src/main/java/mamei/backend/db/controller/DBStatusController.java
@@ -4,8 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/status")
+@RestController
 public class DBStatusController {
 
 
