fd3a058b5900f859da72b2a1e35d6eb37c06c00e
magantus@googlemail.com <Extrem93!>
2023-09-23 12:00:35 +0200

    update

--- backend/src/main/java/mamei/backend/BackendApplication.java
+++ backend/src/main/java/mamei/backend/BackendApplication.java
@@ -2,7 +2,6 @@ package mamei.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BackendApplication {
