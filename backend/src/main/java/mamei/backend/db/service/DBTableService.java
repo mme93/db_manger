bf1bb44922c573fa37a81c2fc248cb3ae78da5e6
magantus@googlemail.com <Extrem93!>
2023-09-24 23:46:14 +0200

    update

--- backend/src/main/java/mamei/backend/db/service/DBTableService.java
+++ backend/src/main/java/mamei/backend/db/service/DBTableService.java
@@ -1,7 +1,32 @@
 package mamei.backend.db.service;
 
+import mamei.backend.db.utility.DBSettingsUtility;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DBTableService {
+
+    private final DBSettingsUtility dbSettingsUtility;
+
+    public DBTableService(DBSettingsUtility dbSettingsUtility) {
+        this.dbSettingsUtility = dbSettingsUtility;
+    }
+
+    public void getAllTablesFromDatabase(String database){
+
+    }
+    public void getTableFromDatabase(String database, String table){
+
+    }
+    public void createTable(String database, String table){
+
+    }
+    public void dropTable(String database, String table){
+
+    }
+    public void updateTable(){
+
+    }
+
+
 }
