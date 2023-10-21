647cd5ff7858323e1bae1521800dab0174e14fe2
magantus@googlemail.com <Extrem93!>
2023-10-21 21:01:43 +0200

    update

---
+++ backend/src/main/java/mamei/backend/db/mariadb/html/controller/HTMLLoginController.java
@@ -0,0 +1,14 @@
+package mamei.backend.db.mariadb.html.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HTMLLoginController {
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String loginPage(){
+        return "css/loginPage";
+    }
+}
