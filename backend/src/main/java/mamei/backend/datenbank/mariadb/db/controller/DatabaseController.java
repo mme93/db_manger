2248fb4c433e0593c5a3286312f97ca5443b7905
magantus@googlemail.com <Extrem93!>
2023-10-22 21:00:39 +0200

    update

--- backend/src/main/java/mamei/backend/datenbank/mariadb/db/controller/DatabaseController.java
+++ backend/src/main/java/mamei/backend/datenbank/mariadb/db/controller/DatabaseController.java
@@ -3,10 +3,7 @@ package mamei.backend.datenbank.mariadb.db.controller;
 import mamei.backend.datenbank.mariadb.db.service.DatabaseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -30,5 +27,4 @@ public class DatabaseController {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
     }
-
 }
--- backend/src/main/java/mamei/backend/datenbank/mariadb/html/controller/WebPageController.java
+++ backend/src/main/java/mamei/backend/datenbank/mariadb/html/controller/WebPageController.java
@@ -5,15 +5,16 @@ import mamei.backend.datenbank.mariadb.html.service.OverviewPageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/web")
-public class WebOverviewPageController {
+public class WebPageController {
 
     private final OverviewPageService overviewPageService;
 
-    public WebOverviewPageController(OverviewPageService overviewPageService) {
+    public WebPageController(OverviewPageService overviewPageService) {
         this.overviewPageService = overviewPageService;
     }
 
@@ -21,6 +22,13 @@ public class WebOverviewPageController {
     public String overviewPage(Model model) {
         model.addAttribute("serverNameList", overviewPageService.getServerNameList());
         return "html/overviewPage";
+    }
+
+    @GetMapping("/tables/{serverName}/{databaseName}")
+    public String getDatabaseOverview(@PathVariable String databaseName, @PathVariable String serverName, Model model){
+        model.addAttribute("serverName", serverName);
+        model.addAttribute("databaseName", databaseName);
+        return "html/databaseOverviewPage";
     }
 
 }
