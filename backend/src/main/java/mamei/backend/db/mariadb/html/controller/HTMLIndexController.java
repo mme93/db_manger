3d0f16730140ac9857f7f5cdb0c73ccf2de91613
magantus@googlemail.com <Extrem93!>
2023-10-22 18:49:26 +0200

    update

--- backend/src/main/java/mamei/backend/db/mariadb/html/controller/HTMLIndexController.java
+++ backend/src/main/java/mamei/backend/db/mariadb/html/controller/HTMLIndexController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/home")
